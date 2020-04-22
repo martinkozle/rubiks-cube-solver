91d0f57c122640778e9c24ab4577c2b74e11df54
martinkozle <martinkozle@yahoo.com>
2020-04-22 19:37:59 +0200

    Created solvers_222.py, added state tree generation so far

--- solvers_222.py
+++ solvers_222.py
@@ -1,5 +1,7 @@
 from cubes import Cube222
 import time
+import queue
+import json
 
 
 class BruteForceSolver:
@@ -24,18 +26,60 @@ class BruteForceSolver:
                 color_perm = color_perm[0] + color_perm[4] + color_perm[1:4] + color_perm[5]
             return base_states
 
-    def generate_state_tree(self):
-        print('Generating state tree...')
-        start_time = time.time()
-        base_states = self._generate_base_states()
+    @staticmethod
+    def _inverse_move(move):
+        return move + "'" if len(move) == 1 else move[0]
 
-        end_time = time.time()
-        print('Finished in {:.3} seconds'.format(end_time - start_time))
+    def generate_state_tree(self, *, log=True):
+        if log:
+            print('Generating state tree...')
+        start_time = time.time()
+        progress = 0
+
+        base_states = ['YYYYRRGGOOBBRRGGOOBBWWWW']
+        valid_moves = ["R", "R'", "F", "F'", "U", "U'"]  # by natural order
+        q = queue.Queue()
+        for base_state in base_states:
+            q.put((base_state, '', '', 0))
+
+        while not q.empty():
+            state, fr, move, depth = q.get()
+            if state not in self._state_dict:
+                self._state_dict[state] = (fr, move)
+                progress += 1
+                if log and progress % 10000 == 0:
+                    print('States generated: ' + str(progress))
+                    print('Current depth: ' + str(depth))
+                for valid_move in valid_moves:
+                    cube = Cube222(state)
+                    cube.make_move(valid_move)
+                    q.put((cube.hash(), state, self._inverse_move(valid_move), depth + 1))
+
+        if log:
+            end_time = time.time()
+            print('Finished in {:.3} seconds'.format(end_time - start_time))
+        print(len(self._state_dict.keys()))
+
+    def save_state_tree(self, file_name):
+        with open(file_name, mode='w') as file:
+            json.dump(self._state_dict, file)
+
+    def load_state_tree(self, file_name):
+        with open(file_name, mode='r') as file:
+            self._state_dict = json.load(file)
 
 
 def main():
     solver = BruteForceSolver()
-    solver.generate_state_tree()
+    if input('Do you want to load from an existing json file?(Y/N) ').lower() in ('y', 'yes', 'ye', 'yea', 'yeah'):
+        file_name = input('Enter file name: ')
+        solver.load_state_tree(file_name)
+    else:
+        solver.generate_state_tree()
+        if input('Do you want to save generated state tree?(Y/N) ').lower() in ('y', 'yes', 'ye', 'yea', 'yeah'):
+            file_name = input('Enter file name (with extension): ')
+            solver.save_state_tree(file_name)
+
 
 
 if __name__ == '__main__':
